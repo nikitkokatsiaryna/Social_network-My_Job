89aa5d8da43cd51ff7cc05f4d9ef92d7bab3202c
Katya Nikitko <katya.nikitko@yandex.ru>
2020-10-02 12:05:51 +0300

    change color

--- blog/form/experience.py
+++ blog/form/experience.py
@@ -6,7 +6,7 @@ class ExperienceForm(forms.ModelForm):
     class Meta:
         model = Experience
         fields = '__all__'
-        exclude = ('user',)
+        exclude = ('user', 'checkbox')
         employment_choices = (
             ('FD', 'Full work day'),
             ('PE', 'Partial employment'),
@@ -19,8 +19,8 @@ class ExperienceForm(forms.ModelForm):
         widgets = {
             'type_employment': forms.Select(choices=employment_choices,
                                             attrs={'class': 'form-control'}),
-            'date_start': forms.SelectDateWidget(attrs={'class': 'form-calendar'}),
-            'date_end': forms.SelectDateWidget(attrs={'class': 'form-calendar'}),
+            'date_start': forms.SelectDateWidget(attrs={'class': 'form-calendar'}, years=range(1900, 2100)),
+            'date_end': forms.SelectDateWidget(attrs={'class': 'form-calendar'}, years=range(1900, 2100)),
             'checkbox': forms.CheckboxInput(attrs={'class': 'checkbox'}),
             'description': forms.Textarea(attrs={'class': 'form-control'})
         }
@@ -31,7 +31,7 @@ class ExperienceForm(forms.ModelForm):
             'company': 'Company',
             'region': 'Region',
             'checkbox': "I'm currently working in this position",
-            'date_start': 'Date starttt',
+            'date_start': 'Date start',
             'date_end': 'Date end',
             'description': 'Description',
         }
